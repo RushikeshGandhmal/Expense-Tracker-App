cb77636fd2fee0dc41a350683c15125967aa8826
RushikeshGandhmal <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-07-27 19:48:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,12 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date(2021, 5, 12),
   },
+  {
+    id: "e5",
+    title: "Headphones",
+    amount: 999,
+    date: new Date(2022, 5, 12),
+  },
 ];
 
 const App = () => {
