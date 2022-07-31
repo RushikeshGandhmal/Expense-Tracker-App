8a8446957a3e7eae4e51547fb5556e5880d07b99
RushikeshGandhmal <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-07-31 22:56:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
+    
     setExpenses((prevExpenses) => {
       return [expense, ...expenses];
     });
