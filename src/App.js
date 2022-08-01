fd75baf083c042c1d0a5dd22dcc3aea89e6e22da
RushikeshGandhmal <60548902+RushikeshGandhmal@users.noreply.github.com>
2022-08-01 23:39:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const DUMMY_EXPENSES = [
     id: "e1",
     title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2021, 7, 14),
+    date: new Date(2020, 7, 14),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
