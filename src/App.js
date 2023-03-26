79a4069b1f292bb61ff72c6a34c166c6f1dc0b2b
Rushi <rushikeshgandhmal@gmail.com>
2023-03-26 13:52:51 +0530

    Rebulding

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
-import { useState } from "react";
-import "./App.css";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
-
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...expenses];
-    });
-  };
-
+function App() {
   return (
     <div>
-      <NewExpense addExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <h1>Hello</h1>
     </div>
-  );
-};
+  )
+}
 
 export default App;
