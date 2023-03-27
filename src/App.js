ff821677ca7fec26b18c9bfde6545ac1e6a5e949
Rushi <rushikeshgandhmal@gmail.com>
2023-03-27 23:48:53 +0530

    Component organization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>change</button>
     </Card>
   );
 }
