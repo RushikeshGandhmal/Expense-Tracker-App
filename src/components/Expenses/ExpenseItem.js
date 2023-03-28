b5ead4c77d0a9953c03e338473a2f4ec967fde86
Rushi <rushikeshgandhmal@gmail.com>
2023-03-28 13:44:16 +0530

    added form for user inout

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,11 +6,6 @@ import { useState } from "react";
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -18,7 +13,6 @@ function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>change</button>
     </Card>
   );
 }
