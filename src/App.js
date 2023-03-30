2c6358ccda486e05341ad6116a55e4d8af7aef4b
Rushi <rushikeshgandhmal@gmail.com>
2023-03-30 20:47:16 +0530

    Yearly Expense Tracker App completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const DUMMY_EXPENSES = [
     id: "e1",
     title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2023, 7, 14),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
@@ -21,6 +21,18 @@ const DUMMY_EXPENSES = [
     title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "The Book",
+    amount: 12,
+    date: new Date(2022, 7, 12),
+  },
+  {
+    id: "e6",
+    title: "Laptop",
+    amount: 350,
+    date: new Date(2023, 3, 12),
   },
 ];
 
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -14,7 +14,6 @@ const ExpensesFilter = (props) => {
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
-          <option value="2019">2019</option>
         </select>
       </div>
     </div>
